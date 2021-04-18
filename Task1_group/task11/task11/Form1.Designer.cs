0b57ed4056636247da3f966d5757c6e63645d5fd
Anton Andryakov <60717352+Drentmort@users.noreply.github.com>
2021-04-18 22:59:18 +0300

    first try

---
+++ Task1_group/task11/task11/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿using System.Windows.Forms;
+
+namespace task11
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            task11.RectanDrawer rectanDrawer1 = new task11.RectanDrawer();
+            task11.EllipseDrawer ellipseDrawer1 = new task11.EllipseDrawer();
+            task11.PolylineDrawer polylineDrawer1 = new task11.PolylineDrawer();
+            task11.BeziersDrawer beziersDrawer1 = new task11.BeziersDrawer();
+            task11.Fundamental0Drawer fundamental0Drawer1 = new task11.Fundamental0Drawer();
+            task11.Fundamental1Drawer fundamental1Drawer1 = new task11.Fundamental1Drawer();
+            task11.Fundamental3Drawer fundamental3Drawer1 = new task11.Fundamental3Drawer();
+            task11.PolygonDrawer polygonDrawer1 = new task11.PolygonDrawer();
+            task11.CloseCurveDrawer closeCurveDrawer1 = new task11.CloseCurveDrawer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.figureCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.isFill = new System.Windows.Forms.CheckBox();
+            this.brushCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.colorCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(499, 269);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += Panel1_Paint;
+            // 
+            // figureCheckedListBox
+            // 
+            this.figureCheckedListBox.CheckOnClick = true;
+            this.figureCheckedListBox.FormattingEnabled = true;
+            this.figureCheckedListBox.Items.AddRange(new object[] {
+            rectanDrawer1,
+            ellipseDrawer1,
+            polylineDrawer1,
+            beziersDrawer1,
+            fundamental0Drawer1,
+            fundamental1Drawer1,
+            fundamental3Drawer1,
+            polygonDrawer1,
+            closeCurveDrawer1});
+            this.figureCheckedListBox.Location = new System.Drawing.Point(525, 12);
+            this.figureCheckedListBox.Name = "figureCheckedListBox";
+            this.figureCheckedListBox.Size = new System.Drawing.Size(173, 184);
+            this.figureCheckedListBox.TabIndex = 0;
+            this.figureCheckedListBox.SelectedIndexChanged += new System.EventHandler(this.figureCheckedListBox_SelectedIndexChanged);
+            // 
+            // isFill
+            // 
+            this.isFill.AutoSize = true;
+            this.isFill.Location = new System.Drawing.Point(525, 222);
+            this.isFill.Name = "isFill";
+            this.isFill.Size = new System.Drawing.Size(69, 17);
+            this.isFill.TabIndex = 2;
+            this.isFill.Text = "Заливка";
+            this.isFill.UseVisualStyleBackColor = true;
+            this.isFill.CheckedChanged += new System.EventHandler(IsFill_CheckedChanged);
+            // 
+            // brushCheckedListBox
+            // 
+            this.brushCheckedListBox.FormattingEnabled = true;
+            this.brushCheckedListBox.Items.AddRange(new object[] {
+            "Сплошная заливка",
+            "Градиент"});
+            this.brushCheckedListBox.Location = new System.Drawing.Point(525, 245);
+            this.brushCheckedListBox.Name = "brushCheckedListBox";
+            this.brushCheckedListBox.Size = new System.Drawing.Size(120, 94);
+            this.brushCheckedListBox.TabIndex = 3;
+            this.brushCheckedListBox.SelectedIndexChanged += new System.EventHandler(this.BrushCheckedListBox_SelectedIndexChanged);
+            // 
+            // colorCheckedListBox
+            // 
+            this.colorCheckedListBox.FormattingEnabled = true;
+            this.colorCheckedListBox.Items.AddRange(new object[] {
+            "Красный",
+            "Оранжевый",
+            "Желтый",
+            "Зеленый",
+            "Голубой",
+            "Синий",
+            "Фиолетовый"});
+            this.colorCheckedListBox.Location = new System.Drawing.Point(12, 275);
+            this.colorCheckedListBox.Name = "colorCheckedListBox";
+            this.colorCheckedListBox.Size = new System.Drawing.Size(117, 94);
+            this.colorCheckedListBox.TabIndex = 4;
+            this.colorCheckedListBox.SelectedIndexChanged += new System.EventHandler(this.ColorCheckedListBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(710, 450);
+            this.Controls.Add(this.colorCheckedListBox);
+            this.Controls.Add(this.brushCheckedListBox);
+            this.Controls.Add(this.isFill);
+            this.Controls.Add(this.figureCheckedListBox);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Task11";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private Panel panel1;
+        private CheckedListBox figureCheckedListBox;
+        private CheckBox isFill;
+        private CheckedListBox brushCheckedListBox;
+        private CheckedListBox colorCheckedListBox;
+    }
+
+
+}
+
---
+++ Task1_group/task11/task11/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace task11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            using (Graphics g = e.Graphics)
+            {
+                foreach(Drawer d in figureCheckedListBox.SelectedItems)
+                {
+                    d.Draw(g);
+                }
+            }
+        }
+
+        private void figureCheckedListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            var list = sender as CheckedListBox;
+            foreach (int index in list.CheckedIndices)
+            {
+                figureCheckedListBox.SetItemChecked(index, false);
+            }
+            figureCheckedListBox.SetItemChecked(list.SelectedIndex, true);
+
+        }
+
+        private void BrushCheckedListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var list = sender as CheckedListBox;
+            foreach (int index in list.CheckedIndices)
+            {
+                brushCheckedListBox.SetItemChecked(index, false);
+            }
+            brushCheckedListBox.SetItemChecked(list.SelectedIndex, true);
+
+            switch (list.SelectedIndex)
+            {
+                case 0:
+                    Gr.brush = new SolidBrush(Gr.color);
+                    break;
+                case 1:
+                    int newR = Gr.color.R + 50 >= 255 ? 255 : Gr.color.R + 50;
+                    int newG = Gr.color.G + 50 >= 255 ? 255 : Gr.color.G + 50;
+                    int newB = Gr.color.B + 50 >= 255 ? 255 : Gr.color.B + 50;
+
+                    Gr.brush = new LinearGradientBrush(new Point(30, 30), new Point(200, 200), Gr.color, 
+                        Color.FromArgb(newR, newG, newB));
+                    break;
+            }
+            panel1.Invalidate();
+        }
+
+        private void ColorCheckedListBox_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            var list = sender as CheckedListBox;
+            foreach (int index in list.CheckedIndices)
+            {
+                colorCheckedListBox.SetItemChecked(index, false);
+            }
+            colorCheckedListBox.SetItemChecked(list.SelectedIndex, true);
+
+            switch (list.SelectedIndex)
+            {
+                case 0:
+                    Gr.color = Color.Red;
+                    break;
+                case 1:
+                    Gr.color = Color.Orange;
+                    break;
+                case 2:
+                    Gr.color = Color.Yellow;
+                    break;
+                case 3:
+                    Gr.color = Color.Green;
+                    break;
+                case 4:
+                    Gr.color = Color.Blue;
+                    break;
+                case 5:
+                    Gr.color = Color.Cyan;
+                    break;
+                case 6:
+                    Gr.color = Color.Violet;
+                    break;
+                    
+            }
+            Gr.brush = new SolidBrush(Gr.color);
+            foreach (int index in brushCheckedListBox.CheckedIndices)
+            {
+                brushCheckedListBox.SetItemChecked(index, false);
+            }
+            brushCheckedListBox.SetItemChecked(0, true);
+            panel1.Invalidate();
+        }
+
+        private void IsFill_CheckedChanged(object sender, EventArgs e)
+        {
+            Gr.isFilled = isFill.Checked;
+            if (isFill.Checked) {
+                brushCheckedListBox.SetItemChecked(0, true);
+                Gr.brush = new SolidBrush(Gr.color);
+            }
+                
+            else
+                foreach (int index in brushCheckedListBox.CheckedIndices)
+                {
+                    brushCheckedListBox.SetItemChecked(index, false);
+                }
+
+            panel1.Invalidate();
+        }
+
+    }
+
+    class Gr
+    {
+        public static Pen pen = new Pen(Color.Black);
+        public static bool isFilled = false;
+        public static Color color = Color.BlueViolet;
+        public static Brush brush;
+    }
+
+
+    abstract class Drawer
+    {
+        public abstract void Draw(Graphics g);
+    }
+
+    class RectanDrawer : Drawer
+    {
+        public override void Draw(Graphics g)
+        {
+            Gr.pen = new Pen(Color.Black);
+            g.DrawRectangle(Gr.pen, 30, 30, 400, 200);
+            if (Gr.isFilled)
+            {
+                g.FillRectangle(Gr.brush, 31, 31, 399, 199);
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return "Прямоугольник";
+        }
+    }
+
+    class EllipseDrawer : Drawer
+    {
+        public override void Draw(Graphics g)
+        {
+            g.DrawEllipse(Gr.pen, 30, 30, 400, 200);
+            if (Gr.isFilled)
+            {
+                g.FillEllipse(Gr.brush, 31, 31, 399, 199);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Эллипс";
+        }
+    }
+
+    class PolylineDrawer : Drawer
+    {
+        public override void Draw(Graphics g)
+        {
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddLine(new Point() { X = 30, Y = 30 }, new Point() { X = 60, Y = 90 });
+            gp.AddLine(new Point() { X = 160, Y = 230 }, new Point() { X = 190, Y = 150 });
+            gp.AddLine(new Point() { X = 210, Y = 230 }, new Point() { X = 350, Y = 120 });
+
+            if (Gr.isFilled)
+            {
+                gp.CloseFigure();
+                Region r = new Region(gp);
+                g.FillRegion(Gr.brush,r);
+                return;
+            }
+            g.DrawPath(Gr.pen, gp);
+        }
+
+        public override string ToString()
+        {
+            return "Ломаная";
+        }
+    }
+
+    class BeziersDrawer : Drawer
+    {
+        public override void Draw(Graphics g)
+        {
+            Point[] p = {new Point() {X=30,Y=30},
+                new Point() {X=60,Y=90},
+                new Point() {X=160,Y=230},
+                new Point() {X=190,Y=150},
+                new Point() {X=210,Y=230},
+                new Point() {X=350,Y=120},
+                new Point() {X=430,Y=60}
+            };
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddBeziers(p);
+
+            if (Gr.isFilled)
+            {
+                gp.CloseFigure();
+                Region r = new Region(gp);
+                g.FillRegion(Gr.brush, r);
+                return;
+            }
+            g.DrawPath(Gr.pen, gp);
+        }
+
+        public override string ToString()
+        {
+            return "Кривая Безье";
+        }
+    }
+
+    class Fundamental0Drawer : Drawer
+    {
+        public override void Draw(Graphics g)
+        {
+            Point[] p = {new Point() {X=30,Y=30},
+                new Point() {X=60,Y=90},
+                new Point() {X=160,Y=230},
+                new Point() {X=190,Y=150},
+                new Point() {X=210,Y=230},
+                new Point() {X=350,Y=120},
+                new Point() {X=430,Y=60}
+            };
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddCurve(p,0);
+
+            if (Gr.isFilled)
+            {
+                gp.CloseFigure();
+                Region r = new Region(gp);
+                g.FillRegion(Gr.brush, r);
+                return;
+            }
+            g.DrawPath(Gr.pen, gp);
+        }
+
+        public override string ToString()
+        {
+            return "Фунд. кривая, нат. 0";
+        }
+    }
+
+    class Fundamental1Drawer : Drawer
+    {
+        public override void Draw(Graphics g)
+        {
+            Point[] p = {new Point() {X=30,Y=30},
+                new Point() {X=60,Y=90},
+                new Point() {X=160,Y=230},
+                new Point() {X=190,Y=150},
+                new Point() {X=210,Y=230},
+                new Point() {X=350,Y=120},
+                new Point() {X=430,Y=60}
+            };
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddCurve(p, 1);
+
+            if (Gr.isFilled)
+            {
+                gp.CloseFigure();
+                Region r = new Region(gp);
+                g.FillRegion(Gr.brush, r);
+                return;
+            }
+            g.DrawPath(Gr.pen, gp);
+        }
+
+        public override string ToString()
+        {
+            return "Фунд. кривая, нат. 1";
+        }
+    }
+
+    class Fundamental3Drawer : Drawer
+    {
+        public override void Draw(Graphics g)
+        {
+            Point[] p = {new Point() {X=30,Y=30},
+                new Point() {X=60,Y=90},
+                new Point() {X=160,Y=230},
+                new Point() {X=190,Y=150},
+                new Point() {X=210,Y=230},
+                new Point() {X=350,Y=120},
+                new Point() {X=430,Y=60}
+            };
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddCurve(p, 3);
+
+            if (Gr.isFilled)
+            {
+                gp.CloseFigure();
+                Region r = new Region(gp);
+                g.FillRegion(Gr.brush, r);
+                return;
+            }
+            g.DrawPath(Gr.pen, gp);
+        }
+
+        public override string ToString()
+        {
+            return "Фунд. кривая, нат. 3";
+        }
+    }
+
+    class PolygonDrawer : Drawer
+    {
+        public override void Draw(Graphics g)
+        {
+            Point[] p = {new Point() {X=30,Y=30},
+                new Point() {X=60,Y=90},
+                new Point() {X=160,Y=230},
+                new Point() {X=190,Y=150},
+                new Point() {X=210,Y=230},
+                new Point() {X=350,Y=120},
+                new Point() {X=430,Y=60}
+            };
+            g.DrawPolygon(Gr.pen, p);
+        }
+
+        public override string ToString()
+        {
+            return "Полигон";
+        }
+    }
+
+    class CloseCurveDrawer : Drawer
+    {
+        public override void Draw(Graphics g)
+        {
+            Point[] p = {new Point() {X=30,Y=30},
+                new Point() {X=60,Y=90},
+                new Point() {X=160,Y=230},
+                new Point() {X=190,Y=150},
+                new Point() {X=210,Y=230},
+                new Point() {X=350,Y=120},
+                new Point() {X=430,Y=60}
+            };
+            g.DrawClosedCurve(Gr.pen, p);
+        }
+
+        public override string ToString()
+        {
+            return "Замкнутая кривая";
+        }
+    }
+}
